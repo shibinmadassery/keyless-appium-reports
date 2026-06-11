ec34959ad2637120f1e4cb7f964d0ea2d9311276
shibin <shibin.m@gmail.com>
2026-06-11 15:42:30 +0400

    feature: it will launch lock-dashboard when starting server

--- start-appium.bat
+++ start-appium.bat
@@ -4,6 +4,14 @@ echo Connecting Oppo wirelessly...
 adb connect 192.168.0.140:5555
 adb devices
 echo.
+
+echo Starting Lock Dashboard...
+start "Lock Dashboard" cmd /k "cd /d "C:\Users\shibi\OneDrive - SIRA.Gov.AE\lock-dashboard\lock-dashboard" && npm start"
+
+timeout /t 3 /nobreak > nul
+start http://localhost:3030
+
+echo.
 echo Starting Appium on localhost:4723...
 npx appium
 pause
